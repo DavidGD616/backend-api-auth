db3162393b0b4984b2de57709bb5151d0c4b0489
David <davidguerrerodiaz12@gmail.com>
2022-01-16 20:09:48 -0800

    link recovery

--- services/auth.service.js
+++ services/auth.service.js
@@ -35,27 +35,36 @@ class AuthService {
     };
   }
 
-  async sendMail(email){
+  async sendRecovery(email){
     const user = await service.findByEmail(email);
     if (!user) {
       throw boom.unauthorized();
     }
+    const payload = { sub: user.id };
+    const token = jwt.sign(payload, config.jwtSecret, {expiresIn: '15min'});
+    const link = `http://myfrontend.com/recovery?token=${token}`;
+    await service.update(user.id, {recoveryToken: token});
+    const mail = {
+      from: config.smtpEmail, // sender address
+      to: `${user.email}`, // list of receivers
+      subject: "Email para recuperar contrasena", // Subject line
+      html: `<b>Ingresa a este link => ${link}</b>`, // html body
+    }
+    const rta = await this.sendMail(mail)
+    return rta;
+  }
+
+  async sendMail(infoMail){
     const transporter = nodemailer.createTransport({
       host: 'smtp.gmail.com',
       secure: true, // true for 465, false for other ports
       port: 465,
       auth: {
-        user: 'davidguerrerodiaz12@gmail.com',
-        pass: 'sjybhgnsryfxwujw',
+        user: config.smtpEmail,
+        pass: config.smtpPassword,
       }
     });
-    await transporter.sendMail({
-      from: 'davidguerrerodiaz12@gmail.com', // sender address
-      to: `${user.email}`, // list of receivers
-      subject: "Hello ✔", // Subject line
-      text: "Hello world?", // plain text body
-      html: "<b>Hello world?</b>", // html body
-    });
+    await transporter.sendMail(infoMail);
     return { message: 'mail sent' };
   }
 }
